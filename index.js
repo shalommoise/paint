b4e9591a9d3f483a5236fd646814b2ade56ff92b
Shalommoise <mshalom689@gmail.com>
2020-10-19 21:36:32 +0100

    created invert and change color buttons

--- index.js
+++ index.js
@@ -6,19 +6,14 @@ const pickColor=(color)=>{
  
 }
  let count = 5401;
-const pickBackground =(color, clean)=>{
+const pickBackground =(color)=>{
   
   document.getElementsByClassName("color")[1].style.background = color;
-  const dot = document.getElementsByClassName("dot");
-for (let i = 0; i < 5400; i++){
-  dot[i].style.background = color;
-  
-}
 
- for (const item in drawings){
-   clean ? delete drawings[item] :
-  document.getElementById(item).style.background = drawings[item]; 
-}
+const canvas = document.getElementsByClassName("canvas")[0];
+
+canvas.style.background = color;
+
 }
 const drawings = {}
 
@@ -46,3 +41,59 @@ create(count);
 }
 
 }
+
+const invert =()=>{
+  
+  const backgroundColor =document.getElementById("backgroundcolor").style.background;
+  const penColor = document.getElementById("color").style.background;
+
+ for (const item in drawings){
+
+  document.getElementById(item).style.background = backgroundColor; 
+}
+document.getElementsByClassName("canvas")[0].style.background = penColor;
+document.getElementById("backgroundcolor").style.background = penColor;
+document.getElementById("color").style.background = backgroundColor;
+}
+
+
+const restart = ()=>{
+  document.getElementsByClassName("canvas")[0].style.background = "white";
+ const arr =  document.getElementsByClassName("dot")
+for (let i =0; i < arr.length;i++){
+  arr[i].style.background = "none";
+}
+
+}
+let changeCount = 0;
+const changeColor = (color, num)=>{
+  let original = document.getElementById("original");
+  let changed = document.getElementById("changed");
+
+changeCount % 2 === 0 ? original.style.background = color: changed.style.background = color;
+changeCount++;
+}
+const showChangeBar =()=>{
+  const section = document.getElementById("changeColorsection");
+  section.style.display === "block" ?  section.style.display = "none" :  section.style.display = "block" 
+
+}
+const activateChange =()=>{
+  let original = document.getElementById("original").style.background;
+  let changed = document.getElementById("changed").style.background;
+  let warning = document.getElementById("changeWarning");
+  if (!original || !changed) {
+  
+   warning.innerHTML = "<i>Please select colors</i>";
+  } else {
+    warning.innerHTML = "";
+    for (const item in drawings){
+      if(drawings[item] === original){ 
+       drawings[item] = changed;
+        document.getElementById(item).style.background =  changed
+    };
+    }
+    document.getElementById("original").style.background = "none";
+    document.getElementById("changed").style.background = "none";
+  }
+} 
